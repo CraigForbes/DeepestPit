fb2aec4c9d48ee5624cf7e8ce4a6d4c6f1614bef
Craig <craigforbes@outlook.com>
2016-09-15 17:58:25 +0200

    Initial Commit

---
+++ DeepestPitTests/DeepestPitTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DeepestPit;
+
+namespace DeepestPitTests
+{
+    [TestClass]
+    public class DeepestPitTests
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+
+            int[] array = new int[] { 0, 1, 3, -2, 0, 1, 0, -3, 2, 3 };
+
+            var solution = new DeepestPitSolution();
+            var result = solution.Solution(array);
+
+            Assert.AreEqual(4, result);
+        }
+    }
+}
